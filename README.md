89e382b7adc27ee8e6cd59d673615c8b11380ee7
Ángel Cámara Redondo <a.camarare@alumnos.urjc.es>
2017-03-16 14:22:13 +0100

    Terminado

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
-# X-NAV-JQ-Ajax
+# Ejericio 5.6.5. Ejemplos simples con Ajax
 Ejercicio de asignaturas de aplicaciones HTML5. Tema jQuery. Ejemplos simples con Ajax.
 
 ## Enunciado
-
 Escribe un programa que, una vez está cargado el árbol DOM de una página HTML, realice la petición HTTP de un documento con un texto, y lo incluya en el elemento con un cierto identificador de dicha página HTML. Para realizar la petición del documento, utiliza la función ajax() de jQuery.
 
 Realiza lo mismo, pero ahora de forma que la petición del documento se realice cuando el usaurio pulsa el ratón sobre otro elemento.
+
+## Resultado
+[http://acamara7es.github.io/X-NAV-JQ-Ajax/index.html](http://acamara7es.github.io/X-NAV-JQ-Ajax/index.html)
---
+++ script.js
@@ -0,0 +1,35 @@
+jQuery(document).ready(function(){
+	$.ajax({
+		url:"text2.txt",
+		type: "GET",
+		cache: false
+	})
+	.done(function(data){
+		$("#auto_load").html(data);
+	});
+    $("#ajax_button").click(function(){
+        $.ajax({
+            url:"text.txt",
+            type: "GET",
+            cache: false
+        })
+        .done(function(data){
+            $("#ajax").html(data);
+        })
+    });
+    $("#ajax").click(function(){
+        $.ajax({
+            url:"text3.txt",
+            type: "GET",
+            cache: false
+        })
+        .done(function(data){
+            $("#ajax").html(data);
+        })
+        .fail(function(data){
+            $("#ajax").html("File not found");
+        }).always(function(data){
+			alert("NUEVOS DATOS");
+		})
+    });
+});
